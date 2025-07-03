e4b494e8e8a5430e400af4ae476a4b8e999aa46d
imper2406 <73357887+imper2406@users.noreply.github.com>
2025-07-03 18:43:19 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 def main():
     print('My first git program')
     print('Hello')
+    print('Я очень прошу прощения, что вовремя не смог завершить курс, но я работаю над этим. Я не сдаюсь')
 
 
 if __name__ == '__main__':
